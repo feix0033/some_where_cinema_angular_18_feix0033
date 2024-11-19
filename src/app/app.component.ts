417efad9ae685c08c4c82afb8b0a935666badae7
feix0033 <evensnachi1@gmail.com>
2024-11-19 17:48:02 +0100

    add the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from "./app-layout/app-header/app-header.component";
+import { AppBodyComponent } from "./app-layout/app-body/app-body.component";
+import { AppFooterComponent } from "./app-layout/app-footer/app-footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeaderComponent, AppBodyComponent, AppFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
