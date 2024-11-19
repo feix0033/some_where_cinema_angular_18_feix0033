7367af21817d7cef7ad18548294bac0deeb0a163
feix0033 <evensnachi1@gmail.com>
2024-11-19 17:36:28 +0100

    make the layout

---
+++ src/app/app-layout/app-body/app-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-body.component.html',
+  styleUrl: './app-body.component.css'
+})
+export class AppBodyComponent {
+
+}
